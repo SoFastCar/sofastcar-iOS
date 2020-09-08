dfd9648bf235684eb742ace76ebdc3df1a5b77c7
devyhan93 <devyhan93@gmail.com>
2020-09-08 22:41:09 +0900

    docs: create VehicleCheckVC
    - 대여 차량 확인 플로우를 위한 VehicleCheckVC 생성

---
+++ sofastcar/sofastcar/ViewController/Reservation/VehicleCheckVC.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleCheckVC.swift
+//  sofastcar
+//
+//  Created by 요한 on 2020/09/08.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+class VehicleCheckVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
