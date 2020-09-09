a8601afdcebfb0720d9fb239b88e6894a87a2835
roobing <roobing11@gmail.com>
2020-09-09 23:05:10 +0900

    feat: 면책 상품 뷰 완료

--- sofastcar/sofastcar/View/Main/SocarZoneInfoButton.swift
+++ sofastcar/sofastcar/View/Main/SocarZoneInfoButton.swift
@@ -53,11 +53,13 @@ class SocarZoneInfoButton: UIButton {
             $0.translatesAutoresizingMaskIntoConstraints = false
         })
         socarZoneNameLabel.snp.makeConstraints({
-            $0.top.equalTo(self).offset(20)
+//            $0.top.equalTo(self).offset(20)
+            $0.centerY.equalTo(self).offset(-15)
             $0.leading.equalTo(self).offset(20)
         })
         socarZoneGroundLevelLabel.snp.makeConstraints({
-            $0.top.equalTo(socarZoneNameLabel.snp.bottom).offset(10)
+//            $0.top.equalTo(socarZoneNameLabel.snp.bottom).offset(10)
+            $0.centerY.equalTo(self).offset(15)
             $0.leading.equalTo(self).offset(20)
             $0.width.equalTo(40)
             $0.height.equalTo(25)
@@ -67,7 +69,8 @@ class SocarZoneInfoButton: UIButton {
             $0.leading.equalTo(socarZoneGroundLevelLabel.snp.trailing).offset(10)
         })
         socarZoneImageView.snp.makeConstraints({
-            $0.top.equalTo(self).offset(20)
+//            $0.top.equalTo(self).offset(20)
+            $0.centerY.equalTo(self)
             $0.trailing.equalTo(self).offset(-20)
             $0.width.equalTo(65)
             $0.height.equalTo(50)
