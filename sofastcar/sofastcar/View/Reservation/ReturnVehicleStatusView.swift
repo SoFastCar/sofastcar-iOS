10ca5aa1e4e8bdcf55273376959a13dc8aba788b
devyhan93 <devyhan93@gmail.com>
2020-09-21 11:12:18 +0900

    docs: create ReturnVehicleStatusView
    - 뷰 컨트롤러와 뷰 파일 분리

---
+++ sofastcar/sofastcar/View/Reservation/ReturnVehicleStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  ReturnVehicleStatusView.swift
+//  sofastcar
+//
+//  Created by 요한 on 2020/09/21.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+class ReturnVehicleStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
