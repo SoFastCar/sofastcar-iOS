d332c5884c8bf5f09f3a9d3f1724f04ee43f1471
devyhan93 <devyhan93@gmail.com>
2020-09-21 10:59:49 +0900

    docs: create ReturnVehicleStatusVC
    - 반납할때 차량이 반납할 준비가 되어있는지 차량의 상태를 보여주는 뷰 컨트롤러

---
+++ sofastcar/sofastcar/ViewController/Reservation/ReturnVehicleStatusVC.swift
@@ -0,0 +1,30 @@
+//
+//  ReturnVehicleStatusVC.swift
+//  sofastcar
+//
+//  Created by 요한 on 2020/09/21.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+class ReturnVehicleStatusVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
