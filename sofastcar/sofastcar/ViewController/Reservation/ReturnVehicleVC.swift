2e016cba3d987078b5935256516f0b29fd3647ab
devyhan93 <devyhan93@gmail.com>
2020-09-22 23:28:32 +0900

    docs: ReturnVehicleVC
    - 반납 프로세스를 위한 ReturnVehicleVC생성

---
+++ sofastcar/sofastcar/ViewController/Reservation/ReturnVehicleVC.swift
@@ -0,0 +1,27 @@
+//
+//  ReturnVehicleVC.swift
+//  sofastcar
+//
+//  Created by 요한 on 2020/09/22.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ReturnVehicleVC: UIViewController {
+  
+  // MARK: - LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setUI()
+  }
+  
+  // MARK: - UI
+  
+  fileprivate func setUI() {
+    view.backgroundColor = .magenta
+  }
+}
