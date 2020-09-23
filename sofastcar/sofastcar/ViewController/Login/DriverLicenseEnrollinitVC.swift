800f50e698b7ae7773a0e38e76ec9b49d5cfcf5b
tootoomaa <tootoomaa@naver.com>
2020-09-23 21:26:14 +0900

    인식 화면 UI 개발

--- sofastcar/sofastcar/ViewController/Login/DriverLicenseEnrollinitVC.swift
+++ sofastcar/sofastcar/ViewController/Login/DriverLicenseEnrollinitVC.swift
@@ -12,13 +12,7 @@ import MobileCoreServices
 class DriverLicenseEnrollinitVC: UIViewController {
   // MARK: - Properties
   let myView = DriverLicenseEnrollinitView()
-  
-  lazy var imagePicker: UIImagePickerController = {
-    let imagePicker = UIImagePickerController()
-    imagePicker.delegate = self
-    return imagePicker
-  }()
-  
+
   // MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -52,48 +46,7 @@ class DriverLicenseEnrollinitVC: UIViewController {
   }
   
   @objc private func tapDriverAuthCompletebutton() {
-    guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
-    imagePicker.sourceType = .camera // sourceType 카메라 선택
-    
-    let mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)
-    
-    imagePicker.mediaTypes = mediaTypes ?? []
-    imagePicker.mediaTypes = ["public.image"]
-    
-    if UIImagePickerController.isFlashAvailable(for: .rear) {
-      imagePicker.cameraFlashMode = .off
-    }
-    
-    present(imagePicker, animated: true, completion: {
-      
-    })
-  }
-  
-}
-
-// MARK: - UIImagePickerControllerDelegate & UINavigationControllerDelegate
-extension DriverLicenseEnrollinitVC: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-  
-  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-    picker.dismiss(animated: true, completion: {
-      let checkDriverLicenseVC = CheckDriverLicenseVC()
-      self.navigationController?.pushViewController(checkDriverLicenseVC, animated: true)
-    })
-  }
-  
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-    if let mediaType = info[.mediaType] as? NSString {
-      if UTTypeEqual(mediaType, kUTTypeImage) {
-        // handle Image Type
-        let originalImage = info[.originalImage] as? UIImage    // 이미지를 가져옴
-        let editedImage = info[.editedImage] as? UIImage        // editedImage
-//        let cripImage = info[.cropRect]  as? UIImage
-        _ = editedImage ?? originalImage
-      }
-    }
-    dismiss(animated: true, completion: {
-      let checkDriverLicenseVC = CheckDriverLicenseVC()
-      self.navigationController?.pushViewController(checkDriverLicenseVC, animated: true)
-    })
+    let newDriverLicenseEnrollinitVC = NewDriverLicenseEnrollinitVC()
+    navigationController?.pushViewController(newDriverLicenseEnrollinitVC, animated: false)
   }
 }
