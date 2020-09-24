b42f9666fd79f81d6ad3832d217d0f3cf11f8066
roobing <roobing11@gmail.com>
2020-09-24 22:36:57 +0900

    feat: 검색 키워드 전부 삭제 시, 검색결과없음 텍스트 보이게함

--- sofastcar/sofastcar/ViewController/Main/SearchVC.swift
+++ sofastcar/sofastcar/ViewController/Main/SearchVC.swift
@@ -13,12 +13,13 @@ import NMapsMap
 class SearchVC: UIViewController {
     
     let searchView = SearchView()
-    //    let socarZoneData = SocarZoneData()
+    lazy var noDataView = UIView(frame: CGRect(x: 0, y: 0, width: searchView.searchResultTableView.bounds.size.width, height: searchView.searchResultTableView.bounds.size.height))
+    let noDataLabel = UILabel()
     var nMapAddress: [NMapAddress]?
     var searchResultData: [Document] = []
     var selectedSearchData: Document?
     lazy var safeArea = self.view.safeAreaLayoutGuide
-    var tempCnt = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,40 +30,7 @@ class SearchVC: UIViewController {
     
     // MARK: - Selector
     @objc func didTapBackButton(_ sender: UIButton) {
-        dissmissSearchVC(isResult: false)
-//        guard let navi = self.presentingViewController as? UINavigationController else { return }
-//        guard let presentingVC = navi.viewControllers.last as? MainVC else { return }
-//        presentingVC.searchVCDismissFlag = true
-//        UIView.animate(withDuration: 0.5, animations: {
-//            
-//            presentingVC.topView.snp.updateConstraints({
-//                $0.top.equalTo(presentingVC.safeArea.snp.top).offset(8)
-//                $0.leading.equalTo(presentingVC.safeArea.snp.leading).offset(10)
-//                $0.trailing.equalTo(presentingVC.safeArea.snp.trailing).offset(-10)
-//                $0.height.equalTo(52)
-//            })
-//            presentingVC.searchView.snp.updateConstraints({
-//                $0.top.equalTo(presentingVC.safeArea.snp.top).offset(8)
-//                $0.leading.equalTo(presentingVC.safeArea.snp.leading).offset(0)
-//                $0.trailing.equalTo(presentingVC.safeArea.snp.trailing).offset(0)
-//                $0.height.equalTo(52)
-//            })
-//            
-//            presentingVC.searchView.shadowContainer.translatesAutoresizingMaskIntoConstraints = false
-//            presentingVC.searchView.shadowContainer.snp.updateConstraints({
-//                $0.height.equalTo(52)
-//            })
-//            
-//            self.searchView.alpha = 0
-//            self.view.backgroundColor = .clear
-//            presentingVC.whiteView.alpha = 0
-//            presentingVC.topView.alpha = 1
-//            presentingVC.searchView.alpha = 0
-//            presentingVC.setBookingTimeButton.frame.origin.y = presentingVC.view.frame.height - presentingVC.setBookingTimeButton.frame.height
-//            presentingVC.view.layoutIfNeeded()
-//        })
-//        presentingVC.dismiss(animated: true)
-        
+        dissmissSearchVC(isResult: false)        
     }
     
     // MARK: - Dissmiss func
@@ -153,9 +121,9 @@ class SearchVC: UIViewController {
 // MARK: - Extension
 extension SearchVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(#function, searchResultData.count)
         if searchResultData.count == 0 {
-            let noDataView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
-            let noDataLabel = UILabel()
+            noDataView.isHidden = false
             noDataLabel.text = "검색한 기록이 없습니다."
             noDataLabel.textColor = UIColor.systemGray2
             noDataView.addSubview(noDataLabel)
@@ -167,7 +135,7 @@ extension SearchVC: UITableViewDataSource {
                 $0.centerY.equalToSuperview().dividedBy(3)
             })
         } else {
-            
+            noDataView.isHidden = true
         }
         return searchResultData.count
     }
@@ -197,35 +165,39 @@ extension SearchVC: UITableViewDelegate {
 
 extension SearchVC: UITextFieldDelegate {    
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        print(#function)
+        print(#function, textField.text)
         guard let inputText = textField.text else { return }
         searchKeyword = inputText
         
-        // 카카오 키워드 장소 검색
-        let url = "https://dapi.kakao.com/v2/local/search/keyword.json?query=\(inputText)&y=\(callPoisition.lat)&x=\(callPoisition.lng)&radius=10000"
-        if let percentageEncodedUrl = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-           let urlTobeRequested = URL(string: percentageEncodedUrl) {
-            print(urlTobeRequested)
-            var urlRequest = URLRequest(url: urlTobeRequested)
-            urlRequest.httpMethod = "GET"
-            urlRequest.addValue("KakaoAK d1fabe4b77743a0a3fe77e60fdd0279c", forHTTPHeaderField: "Authorization")
+        if inputText == "" {
+            searchResultData.removeAll()
+            self.searchView.searchResultTableView.reloadData()
             
-            URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-                guard error == nil else { return print(error)}
-                guard let responseCode = response as? HTTPURLResponse,
-                      responseCode.statusCode == 200 else { return print(response!) }
-                guard let responseData = data else { return print("No data")}
-                do {
-                    let decodedData = try JSONDecoder().decode(SearchResultData.self, from: responseData)
-                    print(decodedData)
-                    self.searchResultData = decodedData.documents
-                    DispatchQueue.main.async {
-                        self.searchView.searchResultTableView.reloadData()
-                    }                
-                } catch {
-                    print("Decode Error")
-                }
-            }.resume()
-        }    
+        } else {
+            // 카카오 키워드 장소 검색
+            let url = "https://dapi.kakao.com/v2/local/search/keyword.json?query=\(inputText)&y=\(callPoisition.lat)&x=\(callPoisition.lng)&radius=10000"
+            if let percentageEncodedUrl = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+               let urlTobeRequested = URL(string: percentageEncodedUrl) {
+                var urlRequest = URLRequest(url: urlTobeRequested)
+                urlRequest.httpMethod = "GET"
+                urlRequest.addValue("KakaoAK d1fabe4b77743a0a3fe77e60fdd0279c", forHTTPHeaderField: "Authorization")
+                
+                URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                    guard error == nil else { return print(error!)}
+                    guard let responseCode = response as? HTTPURLResponse,
+                          responseCode.statusCode == 200 else { return print(response!) }
+                    guard let responseData = data else { return print("No data")}
+                    do {
+                        let decodedData = try JSONDecoder().decode(SearchResultData.self, from: responseData)
+                        self.searchResultData = decodedData.documents
+                        DispatchQueue.main.async {
+                            self.searchView.searchResultTableView.reloadData()
+                        }                
+                    } catch {
+                        print("Decode Error")
+                    }
+                }.resume()
+            }    
+        }
     }
 }
