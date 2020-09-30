4cf4cd4d56415abf64333f4077177c6c15431fff
devyhan93 <devyhan93@gmail.com>
2020-09-30 10:51:00 +0900

    docs: create CoreBluetooth object
    - 코어 블루투스 관련 Class 생성

---
+++ sofastcar/sofastcar/Utility/CoreBluetoothIO.swift
@@ -0,0 +1,13 @@
+//
+//  CoreBluetoothIO.swift
+//  sofastcar
+//
+//  Created by 요한 on 2020/09/30.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import CoreBluetooth
+
+class CoreBluetoothIO: NSObject {
+
+}
