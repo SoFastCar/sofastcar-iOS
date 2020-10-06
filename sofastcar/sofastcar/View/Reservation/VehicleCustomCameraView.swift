ed867b943d9e3b93c7fc44d773b5f512740ff85f
devyhan93 <devyhan93@gmail.com>
2020-10-07 04:36:13 +0900

    feat: added navigationUI
    - 사진 촬영 화면의 네비게이션 UI

--- sofastcar/sofastcar/View/Reservation/VehicleCustomCameraView.swift
+++ sofastcar/sofastcar/View/Reservation/VehicleCustomCameraView.swift
@@ -25,6 +25,30 @@ class VehicleCustomCameraView: UIView {
     return view
   }()
   
+  let closeButton: UIButton = {
+    let button = UIButton()
+    let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .light, scale: .large)
+    let closeSymbol = UIImage(systemName: CommonUI.SFSymbolKey.close.rawValue, withConfiguration: config)
+    button.setImage(
+      closeSymbol,
+      for: .normal
+    )
+//    button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
+    button.tintColor = .white
+    
+    return button
+  }()
+  
+  fileprivate let nowVIewDescriptionLabel: UILabel = {
+    let label = UILabel()
+    label.text = "전면"
+    label.font = UIFont.preferredFont(forTextStyle: .headline)
+    label.textColor = .white
+    label.textAlignment = .center
+    
+    return label
+  }()
+  
   fileprivate let captureDescriptionView: UIView = {
     let view = UIView()
     view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
@@ -90,6 +114,8 @@ class VehicleCustomCameraView: UIView {
       $0.height.equalTo(90)
     }
     
+    navigationUI()
+    
     captureDescriptionView.snp.makeConstraints {
       $0.bottom.equalTo(controlView.snp.top)
       $0.leading.trailing.equalToSuperview()
@@ -125,6 +151,24 @@ class VehicleCustomCameraView: UIView {
       $0.top.equalToSuperview().offset(30)
       $0.centerX.equalToSuperview()
       $0.width.height.equalTo(buttonWidthSize)
+    }
+  }
+  
+  fileprivate func navigationUI() {
+    [closeButton, nowVIewDescriptionLabel].forEach {
+      navigationView.addSubview($0)
+    }
+    
+    closeButton.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(20)
+      $0.top.equalTo(self.safeAreaLayoutGuide)
+      $0.bottom.equalToSuperview()
+    }
+    
+    nowVIewDescriptionLabel.snp.makeConstraints {
+      $0.top.equalTo(self.safeAreaLayoutGuide)
+      $0.bottom.equalToSuperview()
+      $0.centerX.equalToSuperview()
     }
   }
 }
