a0baa07415955aa9cf14e03cae9ff0e2521d6208
devyhan93 <devyhan93@gmail.com>
2020-10-07 04:53:17 +0900

    docs: create CustomCountingLabel
    - UILabel을 커스텀

---
+++ sofastcar/sofastcar/Custom/Label/CustomCountingLabel.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCountingLabel.swift
+//  sofastcar
+//
+//  Created by 요한 on 2020/10/07.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+class CustomCountingLabel: UILabel {
+
+}
